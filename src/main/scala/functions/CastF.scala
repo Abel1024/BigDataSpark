fbe263e07320d4877a9151fbfa7916243aacf789
Oskar <oskar>
2022-03-19 10:14:17 +0100

    Cast function.

--- src/main/scala/functions/CastF.scala
+++ src/main/scala/functions/CastF.scala
@@ -1,5 +1,20 @@
 package functions
 
-class CastF {
+import org.apache.spark.sql.types.{DataType, DataTypes}
+import spark.WithSpark.withSpark
+
+object CastF extends App {
+
+  withSpark { spark =>
+
+    import spark.implicits._
+
+    val df = (1 to 10).toDF
+    df.show()
+
+    df.withColumn("double", $"value".cast(DataTypes.DoubleType)).show()
+    df.withColumn("double", $"value".cast("double")).show()
+
+  }
 
 }
